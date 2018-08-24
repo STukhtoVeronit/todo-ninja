c24274afda15e7a5638e9abab1e9a4b5c9f35963
Stanislau Tukhto <s.tukhto.vironit@vironit.ru>
2018-08-24 14:31:47 +0300

    complete the todo app

--- controllers/todoController/index.js
+++ controllers/todoController/index.js
@@ -1,21 +1,53 @@
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var mongoose = require('mongoose');
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some ass'}];
+var data = [{item: 'buy milk'}, {item: 'walk dog'}, {item: 'kick some ass'}];
+
+//connect to Db
+try {
+    mongoose.connect('mongodb://localhost:27017/nodetodo', { useNewUrlParser: true });
+}catch (e) {
+    console.log(e);
+}
+var todoShema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoShema);
+// DB preparation end
 
 module.exports = function (app) {
+  
+  app.use(bodyParser.urlencoded({
+      extended: true
+  }));
+  app.use(bodyParser.json());
+
+  app.get('/', function (req, res) {
+      Todo.find({}, function (err, data) {
+          if (err) throw err;
+          res.render('todo', {todos: data});
+      });
+  });
 
   app.get('/todo', function (req, res) {
-    res.render('todo', {todos: data});
+    Todo.find({}, function (err, data) {
+        if (err) throw err;
+        res.render('todo', {todos: data});
+    });
   });
 
-  app.post('/todo', urlencodedParser, function (req, res) {
-    console.log("hi" + req.body);
-    data.push(req.body);
-
-    res.json(data);
+  app.post('/todo', function (req, res) {
+    var newTodo = Todo(req.body).save(function (err, data) {
+      if (err) throw err;
+      res.json(data);
+    });
   });
-  app.delete('/todo', function (req, res) {
 
+  app.delete('/todo/:item', function (req, res) {
+    Todo.find({item: req.params.item.replace(/\-/g, '-')}).remove(function (err, data) {
+        if (err) throw err;
+        res.json(data);
+    });
   });
 };
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -1,34 +1,74 @@
-$(document).ready(function(){
+window.onload = function(){
 
-  $('form').on('submit', function(){
-
-    var item = $('form input');
-    var todo = {item: item.val()};
-
-    $.ajax({
-      type: 'POST',
-      url: '/todo',
-      data: todo,
-      success: function(data){
-        //do something with the data via front-end framework
-        location.reload();
-      }
-    });
-
-    return false;
-
+    document.getElementsByTagName('form')[0].addEventListener('submit', function(){
+      var item = document.getElementById('todo-table-form').getElementsByTagName('input')[0];
+      var todo = JSON.stringify({item: item.value});
+      var promise = post('todo', todo);
+      promise.then(function (res) {
+          // console.log(res);
+          // location.reload();
+      }).catch(function (err) {
+          alert(err);
+      });
+      return false;
   });
 
-  $('li').on('click', function(){
-    var item = $(this).text().replace(/ /g, "-");
-    $.ajax({
-      type: 'DELETE',
-      url: '/todo/' + item,
-      success: function(data){
-        //do something with the data via front-end framework
-        location.reload();
-      }
-    });
+  document.getElementById('task-list').addEventListener ('click', function(e){
+    if (e.target && e.target.matches("li")){
+      var item = e.target.textContent.replace(/ /g, "-");
+      var promise = del('todo/' + item);
+      promise.then(function (res) {
+          location.reload();
+      }).catch(function (err) {
+          console.log(err);
+      })
+
+    }
   });
 
-});
+  function post(url, data) {
+      return new Promise(function (resolve, reject) {
+          var xhttp = new XMLHttpRequest();
+          xhttp.open('POST', url, true);
+          xhttp.setRequestHeader('Content-type', 'application/json');
+          xhttp.onload = function () {
+              if (xhttp.status === 200){
+                  try {
+                      resolve(JSON.parse(xhttp.response));
+                  } catch (e) {
+                      resolve(xhttp.response);
+                  }
+              } else {
+                reject(xhttp.statusText);
+              }
+          };
+          xhttp.onerror = function(){
+              reject(xhttp.statusText);
+          };
+          xhttp.send(data);
+      });
+  }
+
+  function del(url) {
+      return new Promise(function (resolve, reject) {
+          var xhttp = new XMLHttpRequest();
+          xhttp.open('DELETE', url, true);
+          xhttp.onload = function () {
+              if (xhttp.status == 200){
+                  try {
+                      resolve(JSON.parse(xhttp.response));
+                  } catch (e) {
+                      resolve(xhttp.response);
+                  }
+              } else {
+                  reject(xhttp.statusText);
+              }
+          };
+          xhttp.onerror = function(){
+              reject(xhttp.statusText);
+          };
+          xhttp.send(null);
+      });
+  }
+
+}
